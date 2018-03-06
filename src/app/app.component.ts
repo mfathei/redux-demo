04903a16c97adf33d2665f7405b5d8c3a064008f
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-06 13:54:15 +0200

    using immutable objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgRedux, select } from 'ng2-redux';
+import { Map } from 'immutable';
+
 import { IAppState } from './store';
 import { INCREMENT } from './actions';
 
@@ -12,9 +14,10 @@ export class AppComponent {
 
   // @select() counter; // direct 
   // @select('counter') count; // another name 
-  @select((s: IAppState) => s.counter) count;// map function and we can use it in coplex states
+  // @select((s: IAppState) => s.counter) count;// map function and we can use it in coplex states
+  @select(s => s.get('counter')) count;
 
-  constructor(private ngRedux: NgRedux<IAppState>) {
+  constructor(private ngRedux: NgRedux<Map<string, any>>) {
   }
 
   increment() {
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { tassign } from 'tassign'
-
+import { Map } from 'immutable';
 import { INCREMENT } from './actions';
 
 export interface IAppState {
@@ -10,12 +10,14 @@ export const INITIAL_STATE: IAppState = {
     counter: 0
 }
 
-export function rootReducer(state: IAppState, action): IAppState {
+export function rootReducer(state: Map<string, any>, action): Map<string, any> {
     switch (action.type) {
         case INCREMENT:
             // return { counter: state.counter + 1 };// using this we need to rewrite all other props in the interface like, prop1: 10, prop2: 'test'
             // return Object.assign({}, state, {counter: state.counter + 1});// we use this to ignore the previous props rewrite problem but we still can add a non-existent property like , isOnline: true
-            return tassign(state, { counter: state.counter + 1 });// best solution as u can't and non-existent props
+            // return tassign(state, { counter: state.counter + 1 });// best solution as u can't and non-existent props
+            // state.set('isOnline', true); same problem so I prefer tassign
+            return state.set('counter', state.get('counter') + 1);
     }
     return state;
 }
