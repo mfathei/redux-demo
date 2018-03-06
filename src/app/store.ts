e0f7cb8b637897ab8949dbc3fdd8d71480a2873d
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-03-06 13:19:07 +0200

    object mutation fix

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,5 @@
+import { tassign } from 'tassign'
+
 import { INCREMENT } from './actions';
 
 export interface IAppState {
@@ -10,7 +12,10 @@ export const INITIAL_STATE: IAppState = {
 
 export function rootReducer(state: IAppState, action): IAppState {
     switch (action.type) {
-        case INCREMENT : return { counter: state.counter + 1 };
+        case INCREMENT:
+            // return { counter: state.counter + 1 };// using this we need to rewrite all other props in the interface like, prop1: 10, prop2: 'test'
+            // return Object.assign({}, state, {counter: state.counter + 1});// we use this to ignore the previous props rewrite problem but we still can add a non-existent property like , isOnline: true
+            return tassign(state, { counter: state.counter + 1 });// best solution as u can't and non-existent props
     }
     return state;
 }
